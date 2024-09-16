33b383274f7cd587ea1a7cea0897d038d9e54670
vikaskarma8080 <vikaskarma8080@gmail.com>
2024-09-16 18:03:02 +0530

    HMS Code

---
+++ src/main/java/com/hms/docterlogin/entities/Medicine.java
@@ -0,0 +1,16 @@
+package com.hms.docterlogin.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "medicines")
+public class Medicine {
+
+}
---
+++ src/main/java/com/hms/entites/Patient.java
@@ -0,0 +1,25 @@
+package com.hms.entites;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String age;
+    private String blood;
+    private String prescription;
+    private String dose;
+    private String fees;
+    private String urgency;
+}
