8e5114f542c2c8949f5e5de52188ec5e4d731983
vikaskarma8080 <vikaskarma8080@gmail.com>
2024-09-16 15:51:43 +0530

    for testing

--- src/main/java/com/hms/testing/Testing.java
+++
@@ -1,8 +0,0 @@
-package com.hms.testing;
-
-public class Testing {
-    public static void main(String[] args) {
-        System.out.println("for testing..");
-        System.out.println("for testing..");
-    }
-}
